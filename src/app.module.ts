2d05abae011897692101d0ed024c7092c0c07d3c
Dimas Bagus Susilo <dimas.susilo@selleri.com>
2021-12-27 18:14:26 +0700

    setup deployment to heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,28 +9,43 @@ import { ClassesModule } from './services/classes/classes.module';
 import { ReportsModule } from './services/reports/reports.module';
 import { StudentsModule } from './services/students/students.module';
 
+const LOCAL_SETUP = TypeOrmModule.forRootAsync({
+  inject: [ConfigService],
+  useFactory: (config: ConfigService) => {
+    return {
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: config.get<string>('DB_USERNAME'),
+      password: config.get<string>('DB_PASSWORD'),
+      database: config.get<string>('DB_NAME'),
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      // synchronize: true,
+      logging: true,
+    };
+  },
+});
+
+const PROD_SETUP = TypeOrmModule.forRootAsync({
+  inject: [ConfigService],
+  useFactory: (config: ConfigService) => {
+    return {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      // synchronize: true,
+      logging: true,
+    };
+  },
+});
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return {
-          type: 'postgres',
-          host: 'localhost',
-          port: 5432,
-          username: config.get<string>("DB_USERNAME"),
-          password: config.get<string>("DB_PASSWORD"),
-          database: config.get<string>("DB_NAME"),
-          entities: [__dirname + '/**/*.entity{.ts,.js}'],
-          // synchronize: true,
-          logging: true
-        };
-      },
-    }),
+    process.env.DATABASE_URL ? PROD_SETUP : LOCAL_SETUP,
     AuthModule,
     UsersModule,
     ClassesModule,
